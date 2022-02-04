2ad3e5ca06d687e84e34d4036ffe0e64778fea6c
zs <zs@bjpowernode.com>
2022-02-04 22:45:12 +0800

    Crm

--- src/main/java/com/njit/crm/workbench/dao/ClueDao.java
+++ src/main/java/com/njit/crm/workbench/dao/ClueDao.java
@@ -1,6 +1,5 @@
 package com.njit.crm.workbench.dao;
 
-import com.bjpowernode.crm.workbench.domain.Clue;
 
 public interface ClueDao {
 
--- src/main/java/com/njit/crm/workbench/service/impl/ClueServiceImpl.java
+++ src/main/java/com/njit/crm/workbench/service/impl/ClueServiceImpl.java
@@ -1,6 +1,11 @@
 package com.njit.crm.workbench.service.impl;
 
+import com.njit.crm.utils.SqlSessionUtil;
+import com.njit.crm.workbench.dao.ClueDao;
 import com.njit.crm.workbench.service.ClueService;
 
 public class ClueServiceImpl implements ClueService {
+
+    private ClueDao clueDao = SqlSessionUtil.getSqlSession().getMapper(ClueDao.class);
+
 }
